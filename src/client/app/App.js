818347f398c798c3955df4b8f029a62e4cd1dcb0
dillonchristensen <Dillongc21@gmail.com>
2017-08-23 10:13:42 -0600

    vid 12

--- src/client/app/App.js
+++ src/client/app/App.js
@@ -2,10 +2,23 @@ import React from 'react';
 import {render} from 'react-dom';
 import ContactsList from './ContactsList'
 
-let contacts = {
+let contacts = [{
+  id: 1,
   name: 'Dillon',
   phone: '555 333 5555'
-}
+}, {
+  id: 2,
+  name: 'Brian',
+  phone: '444 333 5555'
+}, {
+  id: 3,
+  name: 'Alice',
+  phone: '555 333 7777'
+}, {
+  id: 4,
+  name: 'Hank',
+  phone: '111 333 5555'
+}]
 
 class App extends React.Component {
   render () {
